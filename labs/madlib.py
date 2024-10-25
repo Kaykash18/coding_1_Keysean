1a2c63b6d7ce03684a847037c9fbcd0dfa94bec2
Keysean <Keyseantaylor25@winchendon.org>
2024-10-25 11:25:23 -0400

    HW

---
+++ labs/madlib.py
@@ -0,0 +1,4 @@
+#yesterday i was blank. I woke up at blank. when i woke up my mom blank. I proceed to blank
+___=input (can you give me a adjecive)
+Keysean=input ("What is your name?"  
+)
---
+++ labs/trivia.py
@@ -0,0 +1,20 @@
+Q1=input ("What is the weather today in New York City")
+Q2=input ("What day is it today") 
+Q3=input ("How many monkeys were jumping on the bed")
+Q4=input ("How much wood could a woodchuck chuck could a woodchuck could chuck wood")
+Q5=input ("How hard does a person have to laugh to cry")
+if (Q1=="75"):
+        print("correct")
+else:false
+if  (Q2=="Wednesday"):
+        print ("correct")
+else:"false"
+if (Q3=="5"):
+        print ("correct")
+else:"false"
+if (Q4=="wood"):
+        print ("correct")
+else: "false"
+if ( Q5=="very hard"):
+        print ("correct")
+else: "false"
